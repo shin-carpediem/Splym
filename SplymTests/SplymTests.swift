32d9991bee2fa5e9345513d3720fb1c427f1ca2c
shin-carpediem <buru.aoshin@gmail.com>
2022-11-14 22:50:02 +0900

    create project

---
+++ SplymTests/SplymTests.swift
@@ -0,0 +1,20 @@
+import XCTest
+@testable import Splym
+
+final class SplymTests: XCTestCase {
+
+    override func setUpWithError() throws {
+    }
+
+    override func tearDownWithError() throws {
+    }
+
+    func testExample() throws {
+    }
+
+    func testPerformanceExample() throws {
+        self.measure {
+        }
+    }
+
+}
